90a5510fbde1b8fc0ca73e8bbaa1d78da429f289
shubhamsoftwiz <shubham.saini@softwizinfotech.com>
2024-02-01 17:48:29 +0530

    changes

--- src/components/Errors.jsx
+++ src/components/Errors.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Errors = () => {
+  const navigate = useNavigate()
 
+  const backHandler = () =>{
+    navigate('/Demo')
+  }
     
 
   return (
-    <div>errors</div>
+    <div>
+        <p>Error Page</p>
+        <button onClick={backHandler} >Go back</button>
+
+    </div>
   )
 }
 
--- src/components/Two.jsx
+++ src/components/Two.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Two = () => {
+
+
+  const navigate = useNavigate()
+
+  const backHandler = () =>{
+    navigate('/Demo')
+  }
+
+
+  const removeHandler = () =>{
+    localStorage.removeItem("check")
+  }
+
+
   return (
-    <div>Two</div>
+
+    <>
+    <div>This is Page two</div>
+    
+    <button onClick={backHandler} >Go back</button>
+
+  <button onClick={removeHandler} >remove local</button> 
+   
+    </>
   )
 }
 
