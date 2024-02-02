d96092fde69cbb8aaf5512420e818a8c3023f1aa
shubhamsoftwiz <shubham.saini@softwizinfotech.com>
2024-02-02 10:27:16 +0530

    change

--- src/components/One.jsx
+++ src/components/One.jsx
@@ -14,7 +14,7 @@ const One = () => {
   return (
     <div>
 
-      <p>Please Click on "set local" first. then you are able to reach on page two.</p>
+      <p>Please Click on "set local" first. then you are able to reach on page two</p>
         <button onClick={setHandler} >set local</button>
         <button onClick={clickHandler} >Two</button>
 
